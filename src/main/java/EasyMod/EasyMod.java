546f6dc329e8e031ecbf4a0d6ded1675efad2f43
Saizy <ecin6036@gmail.com>
2026-06-20 09:46:10 +0300

    Commit

--- src/main/java/EasyMod/EasyMod.java
+++ src/main/java/EasyMod/EasyMod.java
@@ -12,7 +12,7 @@ public class EasyMod implements ModInitializer {
     // Создание переменной с амлуетом
     public static final Item NOOB_AMULET = new NoobAmulet(new Item.Settings().maxCount(1).group(ItemGroup.MISC));
 
-    // Регистрация амулета в инвентаре
+    // Регистрация амулета
     @Override
     public void onInitialize() {
         Registry.register(Registry.ITEM, new Identifier("easymod", "noob_amulet"), NOOB_AMULET);
